52bd2fc44b8fcdea5c7a19def679c14a8cd4acbf
Chloe Choi <choic925@gmail.com>
2026-05-09 16:28:17 -0500

    Add function for importing assets

--- src/project.py
+++ src/project.py
@@ -1,11 +1,18 @@
 import pygame
 import os
+import sys
+
+def import_assets():
+    #base character
+    base_character = pygame.image.load("assets/oguri.png").convert_alpha()
+    return base_character
 
 def main():
     pygame.init()
     pygame.display.set_caption("Dress-Up Game")
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution)
+    character = import_assets()
     running = True
     while running:
         for event in pygame.event.get():
@@ -15,7 +22,8 @@ def main():
         pygame.display.flip()
     pygame.quit()
 
-#def function_1():
+#TODO: Import assets, clickable UI, sounds
+#character name is oguri
 
 
 #def function_2():
